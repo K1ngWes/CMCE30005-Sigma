d2e19ac0d78b38662ea66233227dd03b9af826d2
Ellen Zhang <zhanac20060313@qq.com>
2026-09-03 08:29:26 +0800

    Colored Vis

--- SME_CBD_Cleaning.R
+++ SME_CBD_Cleaning.R
@@ -1,9 +1,8 @@
 install.packages("ggplot2")
-install.packages("dplyr")
-library(dplyr)
+
 
 business_establishments_and_jobs <- read.csv(
-  "data/business-establishments-and-jobs-data-by-business-size-and-anzsic.csv",
+  "~/Desktop/BAC/business-establishments-and-jobs-data-by-business-size-and-anzsic.csv",
   check.names = FALSE
 )
 
@@ -101,10 +100,44 @@ industry_2024 %>%
     table_body.hlines.style = "solid"
   )
 
+# Summary Statistics
+summary(sme_cbd)      # overall all for DS1
+
+# or just the key variables
+year_summary <- sme_cbd %>%
+  group_by(`Census year`) %>%
+  summarise(
+    total_establishments = sum(`Total establishments`, na.rm = TRUE),
+    total_jobs = sum(`Total jobs`, na.rm = TRUE)
+  )
+
+year_summary
+
+# or 
+names(sme_cbd) <- c(
+  "Year",
+  "Area",
+  "Industry",
+  "Business_size",
+  "Total_establishments",
+  "Total_jobs"
+)
+
+summary(sme_cbd$Total_establishments) # stat table for DS1 total establishment
+summary(sme_cbd$Total_jobs)           # stat table for DS1 total jobs
+
+year_summary <- sme_cbd %>%
+  group_by(Year) %>%
+  summarise(
+    total_establishments = sum(Total_establishments, na.rm = TRUE),
+    total_jobs = sum(Total_jobs, na.rm = TRUE)
+  )
+
+year_summary
 
 
-
-# Vis 1 for all 19 industries: SME establishment trends by industry 
+### VISUALISATION DS1
+### Vis 1 for all 19 industries: SME establishment trends by industry 
 library(ggplot2)
 ggplot(
   industry_trend,
@@ -122,8 +155,86 @@ ggplot(
   ) +
   theme_minimal()
 
+# save
+ggsave(
+  "SME_industry_trends.png",
+  width = 12,
+  height = 8,
+  dpi = 300
+)
 
-# Vis 2 
+### Vis 2: integrate all 19 industries trend over time into same diagram
+library(ggplot2)
 
+ggplot(
+  industry_trend,
+  aes(
+    x = Year,
+    y = `Total establishments`,
+    colour = Industry,
+    group = Industry
+  )
+) +
+  geom_line(linewidth = 1) +
+  labs(
+    title = "SME Establishment Trends by Industry",
+    x = "Year",
+    y = "Total establishments",
+    colour = "Industry"
+  ) +
+  theme_minimal()
 
+# save
+ggsave(
+  "SME_all_industry_trends.png",
+  width = 12,
+  height = 8,
+  dpi = 300
+)
 
+### Vis 3
+# try visualise the absolute changes
+industry_change <- merge(
+  industry_trend[industry_trend$Year == 2002, ],
+  industry_trend[industry_trend$Year == 2024, ],
+  by = "Industry",
+  suffixes = c("_2002", "_2024")
+)
+
+industry_change$Change <-
+  industry_change$`Total establishments_2024` -
+  industry_change$`Total establishments_2002`
+
+# plot: comparing the total SME establishments in each industry in 2002 vs 2024
+# How much did the number of SME establishments change between the first year and the last year?
+library(ggplot2)
+
+ggplot(
+  industry_change,
+  aes(
+    x = reorder(Industry, Change),
+    y = Change
+  )
+) +
+  geom_col(
+    aes(fill = Change > 0)
+  ) +
+  scale_fill_manual(
+    values = c("TRUE" = "#4CAF50", "FALSE" = "#E57373"),
+    guide = "none"
+  ) +
+  coord_flip() +
+  labs(
+    title = "Change in SME Establishments by Industry, 2002–2024",
+    x = NULL,
+    y = "Change in establishments"
+  ) +
+  theme_minimal()
+
+# save
+ggsave(
+  "SME_industry_change_2002_2024.png",
+  width = 12,
+  height = 8,
+  dpi = 300
+)
